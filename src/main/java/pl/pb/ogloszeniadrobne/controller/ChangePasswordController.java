1c2d2cec006af1f4371cc1b0ccaf8c5e10e250af
Zwidek <82658699+Zwidek@users.noreply.github.com>
2023-03-11 14:11:58 +0100

    advertisement forum

--- src/main/java/pl/pb/ogloszeniadrobne/controller/ChangePasswordController.java
+++ src/main/java/pl/pb/ogloszeniadrobne/controller/ChangePasswordController.java
@@ -1,6 +1,5 @@
 package pl.pb.ogloszeniadrobne.controller;
 
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
--- src/main/java/pl/pb/ogloszeniadrobne/controller/HomeController.java
+++ src/main/java/pl/pb/ogloszeniadrobne/controller/HomeController.java
@@ -1,12 +1,64 @@
 package pl.pb.ogloszeniadrobne.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import pl.pb.ogloszeniadrobne.dto.AdvertisementDto;
+import pl.pb.ogloszeniadrobne.model.Category;
+import pl.pb.ogloszeniadrobne.service.AdvertisementService;
+import pl.pb.ogloszeniadrobne.service.CategoryService;
+
+import java.security.Principal;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static pl.pb.ogloszeniadrobne.util.AdvertisementUtil.CURRENT_PAGE;
+import static pl.pb.ogloszeniadrobne.util.AdvertisementUtil.PAGE_SIZE;
 
 @Controller
 class HomeController {
+    private final CategoryService categoryService;
+    private final AdvertisementService advertisementService;
+
+    @Autowired
+    public HomeController(CategoryService categoryService, AdvertisementService advertisementService) {
+        this.categoryService = categoryService;
+        this.advertisementService = advertisementService;
+    }
+
+    public List<Category> findAllCategories() {
+        return categoryService.findAllCategories();
+    }
     @GetMapping("/")
     String home() {
         return "index";
+    }
+
+    @GetMapping("/category")
+    public String home(Model model,
+                       Principal principal,
+                       @RequestParam Optional<Integer> page,
+                       @RequestParam Optional<Integer> size) {
+        int currentPage = page.orElse(CURRENT_PAGE);
+        int pageSize = size.orElse(PAGE_SIZE);
+        String loggedInUser = principal.getName();
+        Page<AdvertisementDto> advPage = advertisementService.getAllAdvertisements(PageRequest.of(currentPage - 1, pageSize));
+        model.addAttribute("loggedInuser", loggedInUser);
+        model.addAttribute("category", findAllCategories());
+        model.addAttribute("advertisement", advPage);
+        int totalPages = advPage.getTotalPages();
+        if (totalPages > 0) {
+            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+                    .boxed()
+                    .collect(Collectors.toList());
+            model.addAttribute("pageNumbers", pageNumbers);
+        }
+        return "home";
     }
 }
