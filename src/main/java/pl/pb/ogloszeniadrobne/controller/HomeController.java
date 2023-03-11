8494beb18a38a3d65e47a4165008a5f97778a8cf
Zwidek <82658699+Zwidek@users.noreply.github.com>
2023-03-11 14:56:02 +0100

    advertisement forum

--- src/main/java/pl/pb/ogloszeniadrobne/controller/HomeController.java
+++ src/main/java/pl/pb/ogloszeniadrobne/controller/HomeController.java
@@ -3,6 +3,9 @@ package pl.pb.ogloszeniadrobne.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,8 +39,14 @@ class HomeController {
         return categoryService.findAllCategories();
     }
     @GetMapping("/")
-    String home() {
-        return "index";
+    String home(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication.isAuthenticated()) {
+            model.addAttribute("authentication", authentication);
+            return "index";
+        } else {
+            return "index";
+        }
     }
 
     @GetMapping("/category")
